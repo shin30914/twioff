fdeab0f31956b255c5abeb071e8d71f5765cc81c
shin30914 <sakatasandan@gmail.com>
2018-11-27 17:04:16 +0900

    fix login required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   include CustomTwitterHelper
-  before_action :login_required
-
-  private
-
-    def login_required
-      unless user_signed_in?
-        redirect_to user_session_path
-      end
-    end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,5 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   def new
     @relationship = Relationship.new
 
