14a84c64f671dfc6d0df1497114ca4fa3d8e4d3c
shin30914 <sakatasandan@gmail.com>
2018-12-05 18:02:10 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/privacy_policy'
   get 'static_pages/terms_of_use'
   get 'static_pages/inquiry'
-  # get "relationships/new"
-  # get "relationships/index"
   devise_for :users, controllers: {
       omniauth_callbacks: 'users/omniauth_callbacks',
       registrations: 'users/registrations',
