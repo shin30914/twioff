9f75c89b9b8e3ebbf9698374ad4637b3e185cbb9
shin30914 <sakatasandan@gmail.com>
2018-12-05 23:41:22 +0900

    add ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -21,18 +21,24 @@ class RelationshipsController < ApplicationController
   end
 
   def create
-    user = User.find(params[:relationship][:followed_id])
-    current_user.follow(user)
-    if user.following?(current_user)
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow(@user)
+    if @user.following?(current_user)
       flash[:alert] = "おめでとうございます！マッチしました！連絡を取ってみてね！"
     end
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to new_relationship_path }
+      format.js
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to root_path
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to new_relationship_path }
+      format.js
+    end
   end
 
   def index
