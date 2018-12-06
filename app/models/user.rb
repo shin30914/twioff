571725eb293f28122d8a2f1d89b9e19adbdabac1
shin30914 <sakatasandan@gmail.com>
2018-12-06 23:59:39 +0900

    ログイン時にTwitterで自動ツイート

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,10 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  # ユーザーをフォローする
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
   end
 
-  # ユーザーをアンフォローする
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
